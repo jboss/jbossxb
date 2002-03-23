b1d1ba56a2bce9ed594b173ec839dd8655a9090e
ejort <ejort@mycompany.com>
2002-03-23 21:11:00 +0000

    JBoss/JBossMX integration. Also includes a fix to the cluster tests and a more complete ejblink test that currently fails
    git-svn-id: https://anonsvn.jboss.org/repos/common/trunk@41 15291643-af16-0410-b526-a589213603f9

--- src/main/org/jboss/util/jmx/MBeanProxy.java
+++ src/main/org/jboss/util/jmx/MBeanProxy.java
@@ -13,15 +13,28 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.lang.reflect.InvocationHandler;
 
+import java.util.HashMap;
+
+import javax.management.Attribute;
+import javax.management.InstanceNotFoundException;
+import javax.management.MBeanAttributeInfo;
+import javax.management.MBeanInfo;
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
 import javax.management.MalformedObjectNameException;
 
 /**
  * A factory for producing MBean proxies.
- *      
+ *
+ * <p><b>Revisions:</b>
+ * <p><b>20020321 Adrian Brock:</b>
+ * <ul>
+ * <li>Don't process attributes using invoke.
+ * </ul>
+ *
  * @author <a href="mailto:rickard.oberg@telkel.com">Rickard Öberg</a>.
  * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
+ * @author <a href="mailto:adrian.brock@happeningtimes.com">Adrian Brock</a>.
  * @version $Revision$
  */
 public class MBeanProxy
@@ -32,6 +45,9 @@ public class MBeanProxy
 
    /** The name of the object to invoke. */
    private final ObjectName name;
+
+   /** The MBean's attributes */
+   private HashMap attributeMap  = new HashMap();
    
    /**
     * Construct a MBeanProxy.
@@ -48,6 +64,20 @@ public class MBeanProxy
    {
       this.name = name;
       this.server = server;
+        
+      // The MBean's attributes
+      try
+      {
+         MBeanInfo info = server.getMBeanInfo(name);
+         MBeanAttributeInfo[] attributes = info.getAttributes();
+
+         for (int i = 0; i < attributes.length; ++i)
+            attributeMap.put(attributes[i].getName(), attributes[i]);
+      }
+      catch (Exception e)
+      {
+         throw new RuntimeException("Error creating MBeanProxy: " + name);
+      }
    }
        
    /**
@@ -59,6 +89,72 @@ public class MBeanProxy
                         Object[] args)
       throws Throwable
    {
+      String methodName = method.getName();
+
+      // Get attribute
+      if (methodName.startsWith("get") && args == null)
+      {
+         String attrName = methodName.substring(3);
+         MBeanAttributeInfo info = (MBeanAttributeInfo) attributeMap.get(attrName);
+         if (info != null)
+         {
+            String retType = method.getReturnType().getName();
+            if (retType.equals(info.getType()))
+            {
+               try
+               {
+                  return server.getAttribute(name, attrName);
+               }
+               catch (Exception e)
+               {
+                  throw JMXExceptionDecoder.decode(e);
+               }
+            }
+         }
+      }
+
+      // Is attribute
+      else if (methodName.startsWith("is") && args == null)
+      {
+         String attrName = methodName.substring(2);
+         MBeanAttributeInfo info = (MBeanAttributeInfo) attributeMap.get(attrName);
+         if (info != null && info.isIs())
+         {
+            Class retType = method.getReturnType();
+            if (retType.equals(Boolean.class) || retType.equals(Boolean.TYPE))
+            {
+               try
+               {
+                  return server.getAttribute(name, attrName);
+               }
+               catch (Exception e)
+               {
+                  throw JMXExceptionDecoder.decode(e);
+               }
+            }
+         }
+      }
+
+      // Set attribute
+      else if (methodName.startsWith("set") && args != null && args.length == 1)
+      {
+         String attrName = methodName.substring(3);
+         MBeanAttributeInfo info = (MBeanAttributeInfo) attributeMap.get(attrName);
+         if (info != null && method.getReturnType() == Void.TYPE)
+         {
+            try
+            {
+               server.setAttribute(name, new Attribute(attrName, args[0]));
+               return null;
+            }
+            catch (Exception e)
+            {
+               throw JMXExceptionDecoder.decode(e);
+            }
+         }
+      }
+
+      // Operation
       if (args == null) args = new Object[0];
 
       // convert the parameter types to strings for JMX
@@ -70,7 +166,7 @@ public class MBeanProxy
 
       // invoke the server and decode JMX exceptions
       try {
-         return server.invoke(name, method.getName(), args, sig);
+         return server.invoke(name, methodName, args, sig);
       }
       catch (Exception e) {
          throw JMXExceptionDecoder.decode(e);

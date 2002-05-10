6241456452813f36450960c1dc25eaccc11ecb84
user57 <user57@mycompany.com>
2002-05-10 05:10:28 +0000

    o added support to trace calls to System.out and System.err by setting    -Dorg.jboss.logging.util.CategoryStream.trace=true.  When set a stack    trace is dumped along with the message
    git-svn-id: https://anonsvn.jboss.org/repos/common/trunk@75 15291643-af16-0410-b526-a589213603f9

--- src/main/org/jboss/logging/util/CategoryStream.java
+++ src/main/org/jboss/logging/util/CategoryStream.java
@@ -15,100 +15,124 @@ import java.io.PrintStream;
 import org.apache.log4j.Category;
 import org.apache.log4j.Priority;
 
+import org.jboss.logging.XPriority;
+
 /**
  * A subclass of PrintStream that redirects its output to a log4j Category.
  * 
  * <p>This class is used to map PrintStream/PrintWriter oriented logging onto
  *    the log4j Categories. Examples include capturing System.out/System.err
- *    writes.
  *
- *
+ * @version <tt>$Revision$</tt>
  * @author <a href="mailto:Scott_Stark@displayscape.com">Scott Stark</a>.
- * @version $Revision$
+ * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
  */
 public class CategoryStream
     extends PrintStream
 {
-    private Category category;
-    private Priority priority;
-    private boolean inWrite;
-    private boolean issuedWarning;
+   /**
+    * Default flag to enable/disable tracing println calls.
+    * from the system property <tt>org.jboss.logging.util.CategoryStream.trace</tt>
+    * or if not set defaults to <tt>false</tt>.
+    */
+   public static final boolean TRACE =
+      getBoolean(CategoryStream.class.getName() + ".trace", false);
 
-    /**
-     * Redirect logging to the indicated category using Priority.INFO
-     */
-    public CategoryStream(final Category category)
-    {
-        this(category, Priority.INFO, System.out);
-    }
+   /** Helper to get boolean value from system property or use default if not set. */
+   private static boolean getBoolean(String name, boolean defaultValue)
+   {
+      String value = System.getProperty(name, null);
+      if (value == null)
+         return defaultValue;
+      return new Boolean(value).booleanValue();
+   }
+   
+   private Category category;
+   private Priority priority;
+   private boolean inWrite;
+   private boolean issuedWarning;
+   
+   /**
+    * Redirect logging to the indicated category using Priority.INFO
+    */
+   public CategoryStream(final Category category)
+   {
+      this(category, Priority.INFO, System.out);
+   } 
     
-    /**
-     * Redirect logging to the indicated category using the given
-     * priority. The ps is simply passed to super but is not used.
-     */
-    public CategoryStream(final Category category,
-                          final Priority priority,
-                          final PrintStream ps)
-    {
-        super(ps);
-        this.category = category;
-        this.priority = priority;
-    }
+   /**
+    * Redirect logging to the indicated category using the given
+    * priority. The ps is simply passed to super but is not used.
+    */
+   public CategoryStream(final Category category,
+                         final Priority priority,
+                         final PrintStream ps)
+   {
+      super(ps);
+      this.category = category;
+      this.priority = priority;
+   }
     
-    public void println(String msg)
-    {
-        if( msg == null )
-            msg = "null";
-        byte[] bytes = msg.getBytes();
-        write(bytes, 0, bytes.length);
-    }
+   public void println(String msg)
+   {
+      if( msg == null )
+         msg = "null";
+      byte[] bytes = msg.getBytes();
+      write(bytes, 0, bytes.length);
+   }
     
-    public void println(Object msg)
-    {
-        if( msg == null )
-            msg = "null";
-        byte[] bytes = msg.toString().getBytes();
-        write(bytes, 0, bytes.length);
-    }
+   public void println(Object msg)
+   {
+      if( msg == null )
+         msg = "null";
+      byte[] bytes = msg.toString().getBytes();
+      write(bytes, 0, bytes.length);
+   }
     
-    public void write(byte b)
-    {
-        byte[] bytes = {b};
-        write(bytes, 0, 1);
-    }
+   public void write(byte b)
+   {
+      byte[] bytes = {b};
+      write(bytes, 0, 1);
+   }
     
-    public synchronized void write(byte[] b, int off, int len)
-    {
-        if( inWrite == true )
-        {
-            // There is a configuration error that is causing looping. Most
-            // likely there are two console appenders so just return to prevent
-            // spinning.
-            if( issuedWarning == false )
+   public synchronized void write(byte[] b, int off, int len)
+   {
+      if( inWrite == true )
+      {
+         // There is a configuration error that is causing looping. Most
+         // likely there are two console appenders so just return to prevent
+         // spinning.
+         if( issuedWarning == false )
+         {
+            String msg = "ERROR: invalid console appender config detected, console stream is looping";
+            try
             {
-                String msg = "ERROR: invalid console appender config detected, console stream is looping";
-                try
-                {
-                    out.write(msg.getBytes());
-                }
-                catch(IOException ignore) {}
-                issuedWarning = true;
+               out.write(msg.getBytes());
             }
-            return;
-        }
-        inWrite = true;
+            catch(IOException ignore) {}
+            issuedWarning = true;
+         }
+         return;
+      }
+      inWrite = true;
         
-        // Remove the end of line chars
-        while( len > 0 && (b[len-1] == '\n' || b[len-1] == '\r') && len > off )
-            len --;
+      // Remove the end of line chars
+      while( len > 0 && (b[len-1] == '\n' || b[len-1] == '\r') && len > off )
+         len --;
 
-        // HACK, something is logging exceptions line by line (including
-        // blanks), but I can't seem to find it, so for now just ignore
-        // empty lines... they aren't very useful.
-        if (len != 0) {
-            String msg = new String(b, off, len);
+      // HACK, something is logging exceptions line by line (including
+      // blanks), but I can't seem to find it, so for now just ignore
+      // empty lines... they aren't very useful.
+      if (len != 0) {
+         String msg = new String(b, off, len);
+
+         if (TRACE) {
+            category.log(priority, msg, new Throwable());
+         }
+         else {
             category.log(priority, msg);
-        }
-        inWrite = false;
-    }
+         }
+      }
+      inWrite = false;
+   }
 }

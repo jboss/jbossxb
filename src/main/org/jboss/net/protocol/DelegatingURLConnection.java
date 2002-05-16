1ff3d36d72945948bd98ad6a2d702ce763a3fba6
user57 <user57@mycompany.com>
2002-05-16 09:16:39 +0000

    o add all of the missing methods from URLConnection so that subclasses    work correctly
    git-svn-id: https://anonsvn.jboss.org/repos/common/trunk@96 15291643-af16-0410-b526-a589213603f9

--- src/main/org/jboss/net/protocol/DelegatingURLConnection.java
+++ src/main/org/jboss/net/protocol/DelegatingURLConnection.java
@@ -11,10 +11,13 @@ package org.jboss.net.protocol;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 import java.net.URLConnection;
 import java.net.URL;
 import java.net.MalformedURLException;
+
+import java.util.Map;
 
 import java.security.Permission;
 
@@ -51,27 +54,148 @@ public class DelegatingURLConnection
       return url.openConnection();
    }
 
-   public void connect() throws IOException {
+   public void connect() throws IOException
+   {
       delegateConnection.connect();
    }
+   
+   public URL getURL() {
+      return delegateConnection.getURL();
+   }
 
-   public Object getContent() throws IOException {
-      return delegateConnection.getContent();
+   public int getContentLength() {
+      return delegateConnection.getContentLength();
    }
 
    public String getContentType() {
       return delegateConnection.getContentType();
    }
 
-   public InputStream getInputStream() throws IOException {
-      return delegateConnection.getInputStream();
+   public String getContentEncoding() {
+      return delegateConnection.getContentEncoding();
    }
 
-   public String getHeaderField(final String name) {
+   public long getExpiration() {
+      return delegateConnection.getExpiration();
+   }
+
+   public long getDate() {
+      return delegateConnection.getDate();
+   }
+
+   public long getLastModified() {
+      return delegateConnection.getLastModified();
+   }
+
+   public String getHeaderField(String name) {
       return delegateConnection.getHeaderField(name);
+   }
+
+   public Map getHeaderFields() {
+      return delegateConnection.getHeaderFields();
+   }
+
+   public int getHeaderFieldInt(String name, int Default) {
+      return delegateConnection.getHeaderFieldInt(name, Default);
+   }
+
+   public long getHeaderFieldDate(String name, long Default) {
+      return delegateConnection.getHeaderFieldDate(name, Default);
+   }
+
+   public String getHeaderFieldKey(int n) {
+      return delegateConnection.getHeaderFieldKey(n);
+   }
+
+   public String getHeaderField(int n) {
+      return delegateConnection.getHeaderField(n);
+   }
+
+   public Object getContent() throws IOException {
+      return delegateConnection.getContent();
+   }
+
+   public Object getContent(Class[] classes) throws IOException {
+      return delegateConnection.getContent(classes);
    }
 
    public Permission getPermission() throws IOException {
       return delegateConnection.getPermission();
+   }
+
+   public InputStream getInputStream() throws IOException {
+      return delegateConnection.getInputStream();
+   }
+
+   public OutputStream getOutputStream() throws IOException {
+      return delegateConnection.getOutputStream();
+   }
+
+   public String toString() {
+      return super.toString() + "{ " + delegateConnection + " }";
+   }
+
+   public void setDoInput(boolean doinput) {
+      delegateConnection.setDoInput(doinput);
+   }
+   
+   public boolean getDoInput() {
+      return delegateConnection.getDoInput();
+   }
+
+   public void setDoOutput(boolean dooutput) {
+      delegateConnection.setDoOutput(dooutput);
+    }
+
+   public boolean getDoOutput() {
+      return delegateConnection.getDoOutput();
+   }
+
+   public void setAllowUserInteraction(boolean allowuserinteraction) {
+      delegateConnection.setAllowUserInteraction(allowuserinteraction);
+   }
+
+   public boolean getAllowUserInteraction() {
+      return delegateConnection.getAllowUserInteraction();
+   }
+
+   public void setUseCaches(boolean usecaches) {
+      delegateConnection.setUseCaches(usecaches);
+   }
+
+   public boolean getUseCaches() {
+      return delegateConnection.getUseCaches();
+   }
+
+   public void setIfModifiedSince(long ifmodifiedsince) {
+      delegateConnection.setIfModifiedSince(ifmodifiedsince);
+   }
+
+   public long getIfModifiedSince() {
+      return delegateConnection.getIfModifiedSince();
+   }
+
+   public boolean getDefaultUseCaches() {
+      return delegateConnection.getDefaultUseCaches();
+   }
+
+   public void setDefaultUseCaches(boolean defaultusecaches) {
+      delegateConnection.setDefaultUseCaches(defaultusecaches);
+   }
+
+   public void setRequestProperty(String key, String value) {
+      delegateConnection.setRequestProperty(key, value);
+   }
+   
+   public void addRequestProperty(String key, String value) {
+      delegateConnection.addRequestProperty(key, value);
+   }
+
+   public String getRequestProperty(String key) {
+      return delegateConnection.getRequestProperty(key);
+   }
+
+   public Map getRequestProperties() {
+      return delegateConnection.getRequestProperties();
    }
 }

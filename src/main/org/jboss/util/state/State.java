e2781bea08e52b80de51e6a7107c25c7e4ec5ae2
user57 <user57@mycompany.com>
2002-07-25 00:36:09 +0000

    o Added somemore javadoc  o Expose name and value as public; they are final so they can't be    mucked with.
    git-svn-id: https://anonsvn.jboss.org/repos/common/trunk@183 15291643-af16-0410-b526-a589213603f9

--- src/main/org/jboss/util/state/State.java
+++ src/main/org/jboss/util/state/State.java
@@ -17,8 +17,8 @@ import org.jboss.util.CloneableObject;
  * Provides the basic interface for states (both accepting, acceptable and final)
  * of a state machine.
  *
- * <p>Each state has a name and integer value.  States are considered equivilent
- *    if the integer values equal.
+ * <p>Each state is immutable and has a name and integer value.  States are
+ *    considered equivilent if the integer values equal.
  *
  * <p>Note that the second opperand is an annonymous class, changing its
  *    equivilence from a vanilla Type instance.
@@ -26,7 +26,7 @@ import org.jboss.util.CloneableObject;
  * <p>State objects also can contain an optional opaque object.  This is provided
  *    for applications to make use of the state machine for managing data
  *    assocciated with the state.
- *      
+ *
  * @version <tt>$Revision$</tt>
  * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
  */
@@ -34,10 +34,17 @@ public class State
    extends CloneableObject
    implements Serializable
 {
-   protected final int value;
+   /**
+    * The integer value of the state.
+    *
+    * <p>States with the same value are equal.
+    */
+   public final int value;
 
-   protected final String name;
+   /** The human readable name of the state. */
+   public final String name;
 
+   /** The optional opaque user data for the state. */
    protected Object opaque;
    
    public State(final int value, final String name)
@@ -56,6 +63,10 @@ public class State
       return value;
    }
 
+   /**
+    * Returns the name of the state, or if null returns the
+    * integer value as a string.
+    */
    public String getName()
    {
       return (name == null ? String.valueOf(value) : name);
@@ -76,16 +87,32 @@ public class State
       return getName();
    }
 
+   /**
+    * Returns the identity string of this state.
+    *
+    * <p>This is the same value returned by {@link Object#toString()}.
+    */
    public String toIdentityString()
    {
       return super.toString();
    }
-   
+
+   /**
+    * Returns the has code of the object.
+    *
+    * @return The integer value of the state.
+    */
    public int hashCode()
    {
       return value;
    }
 
+   /**
+    * Check for state equality.
+    *
+    * <p>States are considered equal if the integer values
+    *    of the State objects are equivilent.
+    */
    public boolean equals(final Object obj)
    {
       if (obj == this) return true;

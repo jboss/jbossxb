3dac3826157b8f1a8da95025c7b38f6454daab96
d_jencks <d_jencks@mycompany.com>
2003-05-11 03:52:34 +0000

    finish Scott's attempt to port 3.2 changes to head, at least for the common module
    git-svn-id: https://anonsvn.jboss.org/repos/common/trunk@381 15291643-af16-0410-b526-a589213603f9

--- src/main/org/jboss/net/protocol/URLLister.java
+++ src/main/org/jboss/net/protocol/URLLister.java
@@ -20,13 +20,36 @@ import java.util.Collection;
 public interface URLLister {
    /**
     * List the members of the given collection URL that match the patterns
-    * supplied.
+    * supplied and, if it contains directory that contains NO dot in the name and
+    * scanNonDottedSubDirs is true, recursively finds URL in these directories.
+    * @param baseUrl the URL to list; must end in "/"
+    * @param patterns the patterns to match (separated by ',')
+    * @param scanNonDottedSubDirs enables recursive search for directories containing no dots
+    * @return a Collection of URLs that match
+    * @throws IOException if there was a problem getting the list
+    */
+   Collection listMembers(URL baseUrl, String patterns, boolean scanNonDottedSubDirs) throws IOException;
+   
+   /**
+    * List the members of the given collection URL that match the patterns
+    * supplied. Doesn't recursively list files contained in directories.
     * @param baseUrl the URL to list; must end in "/"
     * @param patterns the patterns to match (separated by ',')
     * @return a Collection of URLs that match
     * @throws IOException if there was a problem getting the list
     */
    Collection listMembers(URL baseUrl, String patterns) throws IOException;
+
+   /**
+    * List the members of the given collection that are accepted by the filter
+    * @param baseUrl the URL to list; must end in "/"
+    * @param filter a filter that is called to determine if a member should
+    *               be returned
+    * @param scanNonDottedSubDirs enables recursive search for directories containing no dots
+    * @return a Collection of URLs that match
+    * @throws IOException if there was a problem getting the list
+    */
+   Collection listMembers(URL baseUrl, URLFilter filter, boolean scanNonDottedSubDirs) throws IOException;
 
    /**
     * List the members of the given collection that are accepted by the filter
--- src/main/org/jboss/net/protocol/http/DavURLLister.java
+++ src/main/org/jboss/net/protocol/http/DavURLLister.java
@@ -22,34 +22,84 @@ import org.apache.util.HttpURL;
 import org.apache.webdav.lib.WebdavResource;
 import org.jboss.net.protocol.URLListerBase;
 
-public class DavURLLister extends URLListerBase {
-   public Collection listMembers(URL baseUrl, URLFilter filter) throws IOException {
+public class DavURLLister extends URLListerBase
+{
+   public Collection listMembers (URL baseUrl, URLFilter filter) throws IOException
+   {
+      return listMembers (baseUrl, filter, false);
+   }
+
+   public Collection listMembers (URL baseUrl, URLFilter filter, boolean scanNonDottedSubDirs) throws IOException
+   {
       WebdavResource resource = null;
-      try {
-         resource = new WebdavResource(baseUrl.toString());
-         WebdavResource[] resources = resource.listWebdavResources();
-         List urls = new ArrayList(resources.length);
-         for (int i = 0; i < resources.length; i++) {
+      try
+      {
+         resource = new WebdavResource (baseUrl.toString ());
+         WebdavResource[] resources = resource.listWebdavResources ();
+         List urls = new ArrayList (resources.length);
+         for (int i = 0; i < resources.length; i++)
+         {
             WebdavResource member = resources[i];
-            HttpURL httpURL = member.getHttpURL();
-            if (filter.accept(baseUrl, httpURL.getName())) {
-               String url = httpURL.getUnescapedHttpURL();
-               if (member.isCollection() && url.endsWith("/") == false) {
-                  url += "/";
+            HttpURL httpURL = member.getHttpURL ();
+            if (filter.accept (baseUrl, httpURL.getName ()))
+            {
+               String url = httpURL.getUnescapedHttpURL ();
+               if (member.isCollection ())
+               {
+                  if (! url.endsWith ("/"))
+                     url += "/";
+
+                  // it is a directory: do we have to recursively list its content?
+                  //
+                  if (scanNonDottedSubDirs && getFilePartFromUrl (httpURL.toURL ()).indexOf (".") == -1)
+                  {
+                     URL subUrl = new URL (url) ;
+                     urls.addAll (listMembers (subUrl, filter, scanNonDottedSubDirs));
+                  }
+                  else
+                  {
+                     urls.add (new URL (url));
+                  }
                }
-               urls.add(new URL(url));
+               else
+               {
+                  urls.add (new URL (url));
+               }
+               
             }
          }
          return urls;
-      } catch (HttpException e) {
-         throw new IOException(e.getMessage());
-      } catch (MalformedURLException e) {
+      } catch (HttpException e)
+      {
+         throw new IOException (e.getMessage ());
+      } catch (MalformedURLException e)
+      {
          // should not happen
-         throw new IllegalStateException(e.getMessage());
-      } finally {
-         if (resource != null) {
-            resource.close();
+         throw new IllegalStateException (e.getMessage ());
+      } finally
+      {
+         if (resource != null)
+         {
+            resource.close ();
          }
       }
    }
+   
+   protected static final String getFilePartFromUrl (URL source)
+   {
+      String name = source.getPath ();
+      int length = name.length ();
+      
+      if (name.charAt (length - 1) == '/')
+      {
+         int start = name.lastIndexOf ("/", length - 2);
+         return name.substring (start, length -2);
+      }
+      else
+      {
+         int start = name.lastIndexOf ("/");
+         return name.substring (start);         
+      }
+   }
+   
 }

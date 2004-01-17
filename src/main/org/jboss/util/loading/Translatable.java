b55f54d202d3ffc7450d48f72d39d05912d8eb71
starksm <starksm@mycompany.com>
2004-01-17 19:30:03 +0000

    Remove the loadClass methods.
    git-svn-id: https://anonsvn.jboss.org/repos/common/trunk@479 15291643-af16-0410-b526-a589213603f9

--- src/main/org/jboss/util/loading/Translatable.java
+++ src/main/org/jboss/util/loading/Translatable.java
@@ -8,9 +8,14 @@ package org.jboss.util.loading;
 
 import java.net.URL;
 
+/** An interface representing class loader like senamics used in the aop
+ * layer. Its only purpose was to remove the explicit dependency on the
+ * JBoss UCL class loader api, but its existence seems to be a hack that
+ * should be removed.
+ * 
+ * @version $Revision$
+ */ 
 public interface Translatable
 {
-   public Class loadClass(String name) throws ClassNotFoundException;
-   public Class loadClass(String name, byte[] byteCode) throws ClassFormatError;
    public URL getResourceLocally(String name);
 }

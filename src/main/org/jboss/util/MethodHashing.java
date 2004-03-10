4d9e3c8885ef596d9daa3e0d22b9b04c72cc9908
patriot1burke <patriot1burke@mycompany.com>
2004-03-10 19:53:32 +0000

    added constructor caller pointcuts
    git-svn-id: https://anonsvn.jboss.org/repos/common/trunk@515 15291643-af16-0410-b526-a589213603f9

--- src/main/org/jboss/util/MethodHashing.java
+++ src/main/org/jboss/util/MethodHashing.java
@@ -9,6 +9,7 @@ package org.jboss.util;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.lang.reflect.Method;
+import java.lang.reflect.Constructor;
 import java.security.DigestOutputStream;
 import java.security.MessageDigest;
 import java.util.HashMap;
@@ -42,6 +43,20 @@ public class MethodHashing
       return null;
    }
 
+   public static Constructor findConstructorByHash(Class clazz, long hash) throws Exception
+   {
+      Constructor[] cons = clazz.getDeclaredConstructors();
+      for (int i = 0; i < cons.length; i++)
+      {
+         if (constructorHash(cons[i]) == hash) return cons[i];
+      }
+      if (clazz.getSuperclass() != null)
+      {
+         return findConstructorByHash(clazz.getSuperclass(), hash);
+      }
+      return null;
+   }
+
    public static long methodHash(Method method)
       throws Exception
    {
@@ -53,6 +68,29 @@ public class MethodHashing
       }
       methodDesc += ")"+getTypeString(method.getReturnType());
       
+      long hash = 0;
+      ByteArrayOutputStream bytearrayoutputstream = new ByteArrayOutputStream(512);
+      MessageDigest messagedigest = MessageDigest.getInstance("SHA");
+      DataOutputStream dataoutputstream = new DataOutputStream(new DigestOutputStream(bytearrayoutputstream, messagedigest));
+      dataoutputstream.writeUTF(methodDesc);
+      dataoutputstream.flush();
+      byte abyte0[] = messagedigest.digest();
+      for(int j = 0; j < Math.min(8, abyte0.length); j++)
+         hash += (long)(abyte0[j] & 0xff) << j * 8;
+      return hash;
+   }
+
+   public static long constructorHash(Constructor method)
+      throws Exception
+   {
+      Class[] parameterTypes = method.getParameterTypes();
+      String methodDesc = method.getName()+"(";
+      for(int j = 0; j < parameterTypes.length; j++)
+      {
+         methodDesc += getTypeString(parameterTypes[j]);
+      }
+      methodDesc += ")";
+
       long hash = 0;
       ByteArrayOutputStream bytearrayoutputstream = new ByteArrayOutputStream(512);
       MessageDigest messagedigest = MessageDigest.getInstance("SHA");

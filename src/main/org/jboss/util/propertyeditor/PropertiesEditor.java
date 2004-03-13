793488e5a4275a75ff5da2c4e1fbaddf0bb3fb5b
starksm <starksm@mycompany.com>
2004-03-13 18:06:44 +0000

    Update to look for property values that reference other properties using the ${x} syntax. These can be either other properties in the Properties being parsed or a System property.
    git-svn-id: https://anonsvn.jboss.org/repos/common/trunk@521 15291643-af16-0410-b526-a589213603f9

--- src/main/org/jboss/util/propertyeditor/PropertiesEditor.java
+++ src/main/org/jboss/util/propertyeditor/PropertiesEditor.java
@@ -9,25 +9,28 @@
 
 package org.jboss.util.propertyeditor;
 
-import java.util.Properties;
-
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.Properties;
 
 import org.jboss.util.NestedRuntimeException;
+import org.jboss.util.Strings;
 
 /**
  * A property editor for {@link java.util.Properties}.
  *
- * @version <tt>$Revision$</tt>
- * @author  <a href="mailto:jason@planet57.com">Jason Dillon</a>
+ * @author Jason Dillon
+ * @author Scott.Stark@jboss.org
+ * @version $Revision$
  */
 public class PropertiesEditor
    extends TextPropertyEditorSupport
 {
    /**
-    * Returns a Properties object initialized with the input object
-    * as a properties file based string.
+    * Returns a Properties object initialized with current getAsText value
+    * interpretted as a .properties file contents. This replaces any
+    * references of the form ${x} with the corresponding system property. 
     *
     * @return a Properties object
     *
@@ -35,15 +38,31 @@ public class PropertiesEditor
     */
    public Object getValue()
    {
-      try {
-         ByteArrayInputStream is = new ByteArrayInputStream(getAsText().getBytes());
-         Properties p = new Properties();
-         p.load(is);
-      
-         return p;
+      try
+      {
+         // Load the current key=value properties into a Properties object
+         String propsText = getAsText();
+         Properties rawProps = new Properties(System.getProperties());
+         ByteArrayInputStream bais = new ByteArrayInputStream(propsText.getBytes());
+         rawProps.load(bais);
+         // Now go through the rawProps and replace any ${x} refs
+         Properties props = new Properties();
+         Iterator keys = rawProps.keySet().iterator();
+         while( keys.hasNext() )
+         {
+            String key = (String) keys.next();
+            String value = rawProps.getProperty(key);
+            String value2 = Strings.replaceProperties(value, rawProps);
+            props.setProperty(key, value2);
+         }
+         rawProps.clear();
+
+         return props;
       }
-      catch (IOException e) {
+      catch (IOException e)
+      {
          throw new NestedRuntimeException(e);
       }
    }
+
 }

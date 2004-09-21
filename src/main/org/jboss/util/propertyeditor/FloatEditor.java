a279b13c8f8673f85c34635bd0dfd2f969a791b7
patriot1burke <patriot1burke@mycompany.com>
2004-09-21 22:42:53 +0000

    add missing editors
    git-svn-id: https://anonsvn.jboss.org/repos/common/trunk@653 15291643-af16-0410-b526-a589213603f9

---
+++ src/main/org/jboss/util/propertyeditor/FloatEditor.java
@@ -0,0 +1,51 @@
+/***************************************
+
+ *                                     *
+
+ *  JBoss: The OpenSource J2EE WebOS   *
+
+ *                                     *
+
+ *  Distributable under LGPL license.  *
+
+ *  See terms of license at gnu.org.   *
+
+ *                                     *
+
+ ***************************************/
+
+
+package org.jboss.util.propertyeditor;
+
+
+import java.beans.PropertyEditorSupport;
+
+
+/**
+ * A property editor for {@link Integer}.
+ *
+ * @author Scott.Stark@jboss.org
+ * @version $Revision$
+ */
+
+public class FloatEditor extends PropertyEditorSupport
+
+{
+
+   /**
+    * Map the argument text into and Integer using Integer.valueOf.
+    */
+
+   public void setAsText(final String text)
+
+   {
+
+      Object newValue = Float.valueOf(text);
+
+      setValue(newValue);
+
+   }
+
+
+}
+

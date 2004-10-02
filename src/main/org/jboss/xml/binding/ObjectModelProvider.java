925dd92a998b4ea25bb86c8bffe077245155e183
loubyansky <loubyansky@mycompany.com>
2004-10-02 23:31:49 +0000

    updated javadoc
    git-svn-id: https://anonsvn.jboss.org/repos/common/trunk@665 15291643-af16-0410-b526-a589213603f9

--- src/main/org/jboss/xml/binding/ObjectModelProvider.java
+++ src/main/org/jboss/xml/binding/ObjectModelProvider.java
@@ -9,56 +9,74 @@ package org.jboss.xml.binding;
 /**
  * The interface all object model providers must implement. Object model providers are used on marshalling
  * providing data for XML content based on the object model and XML schema or DTD.
- *
- * <p>Each object model provider should have the following methods:
- *
- * <p><code>public Object getRoot()</code>
- * This method returns the root of the object model and is called by the marshaller
- * if the root was not provided in other way, see org.jboss.xml.binding.Marshaller.
- *
- * <p>a set of <code>getChildren()</code> methods.
- * This method is called by the marshaller when the marshalling of a new XML element is started.
- * Each <code>getChildren()</code> method must have three arguments:
+ * <p/>
+ * Each object model provider must implement one method <code>getRoot</code> defined in ObjectModelProvider interface
+ * and a set of getChildren, getElementValue and getAttributeValue methods descovered by the framework at runtime
+ * with introspection.
+ * <p/>So, the following methods should be implemented:
+ * <ul>
+ * <li><code>getRoot</code> method
+ * <pre>
+ *    java.lang.Object getRoot(java.lang.Object o, java.lang.String namespaceURI, java.lang.String localName)
+ * </pre>
+ * This method is called on the object model provider by the framework when a root XML element is marshalled.
+ * The method returns an object that represents the root of the XML content corresponding to the namespace URI and
+ * local name.
+ * </li>
+ * <li>a set of <code>getChildren</code> methods
+ * This method is called on the object model provider by the framework when marshalling of a new XML element started.
+ * Each <code>getChildren</code> method must have three arguments:
  * <ol>
- * <li>the first one is the parent object that is "asked" for its children.
- *     For the root element the object returned by the <code>getRoot()</code> is passed as the parent.</li>
- * <li>the third one is the namespace URI of the child element.</li>
- * <li>the forth one is the local name of the child element.</li>
+ * <li>parent object of a concrete Java type (not java.lang.Object) that is "asked" for its children</li>
+ * <li>namespace URI of the child XML element as java.lang.String</li>
+ * <li>local name of the child element as java.lang.String</li>
  * </ol>
- * A <code>getChildren()</code> method returns children according to the namespace URI and local name.
- * It can return null if there are no children in this object model corresponding to the namespace and local name.
- * If there is only one child corresponding to the namespace and local name then it is returned as is.
- * If there are many children, they are can be returned as an array or java.util.List, or java.util.Collection,
- * or java.util.Iterator.
- *
- * <p>A set of <code>getElementValue()</code> methods.
- * This method is called for objects that represent XML elements with simple content.
- * The method has the following arguments:
+ * A <code>getChildren</code> method returns children that represent the namespace URI and local name in XML content.
+ * The method can return null if there are no children in this object graph corresponding to the namespace and local name.
+ * The method can return a single object if there is only one child object corresponding to the namespace and local name.
+ * If there are many children that match the namespace URI and local name, the method can return them as an array,
+ * java.util.List, java.util.Collection or java.util.Iterator.
+ * </li>
+ * <li>a set of <code>getElementValue</code> methods
+ * This method is called on the object model provider by the framework for objects that represent XML elements with
+ * simple content, i.e. elements that don't contain nested XML elements.
+ * The method must have three arguments:
  * <ol>
- * <li>an object that is "asked" to provide a value corresponding to the other arguments.</li>
- * <li>the namespace URI of the element with simple content.</li>
- * <li>the name of the element with simple content.</li>
+ * <li>an object of a concrete Java type (not java.lang.Object) that is "asked" to provide a value of the XML element
+ * being marshalled</li>
+ * <li>namespace URI as java.lang.String of the XML element being marshalled</li>
+ * <li>local name as java.lang.String of the XML element being marshalled</li>
  * </ol>
- * The method returns either null if the object model does not have any value corresponding to the namespace
- * and local name passed in (in this case the XML content will not contain this element) or the actual value
- * that will appear in XML content.
- *
- * <p>A set of <code>getAttributeValue()</code> methods.
- * This method is called for objects that represent XML elements with attributes.
- * The method has the following arguments:
+ * The method returns either null if the object model does not have any value corresponding to the namespace URI
+ * and local name (in this case the XML content will not contain this XML element) or the actual value of the XML element.
+ * </li>
+ * <li>a set of <code>getAttributeValue</code> methods
+ * This method is called on the object model provider by the framework for objects that represent XML elements with
+ * attributes.
+ * The method must have three arguments:
  * <ol>
- * <li>an object that is "asked" to provide an attribute value corresponding to the other arguments.</li>
- * <li>the namespace URI of the attribute.</li>
- * <li>the local name of the attribute.</li>
+ * <li>an object of a concrete Java type (not java.lang.Object) that is "asked" to provide a value for the XML attribute
+ * being marshalled</li>
+ * <li>namespace URI of the XML attribute being marshalled</li>
+ * <li>local name of the XML attribute being marshalled</li>
  * </ol>
- * The method returns either null if the object model does not have any value corresponding to the namespace
- * and local name passed in (in this case the XML content will not contain this attribute) or the actual value
- * that will appear as the attribute value in XML content.
+ * The method returns either null if the object graph does not have any value corresponding to the namespace URI
+ * and local name (in this case the XML content will not contain this attribute) or the actual value of the XML attribute.
+ * </li>
+ * </ol>
  *
- * @version <tt>$Revision$</tt>
  * @author <a href="mailto:alex@jboss.org">Alexey Loubyansky</a>
+ * @version <tt>$Revision$</tt>
  */
 public interface ObjectModelProvider
 {
+   /**
+    * Called by the framework when a root XML element is marshalled.
+    *
+    * @param o            the root of the object graph
+    * @param namespaceURI namespace URI of the root XML element being marshalled
+    * @param localName    local name of the root XML element being marshalled
+    * @return an object that represents the root XML element corresponding to the namespace URI and local name
+    */
    Object getRoot(Object o, String namespaceURI, String localName);
 }

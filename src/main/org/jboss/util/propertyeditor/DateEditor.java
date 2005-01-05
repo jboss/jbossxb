7e8e44df4d854363d55d506c23cde89a399f8a8f
starksm <starksm@mycompany.com>
2005-01-05 07:59:50 +0000

    Expand the date parsing to expand the formats it supports. It now tries the  following SimpleDateFormat formats in order: * org.jboss.util.propertyeditor.DateEditor.format system property value (or MMM d, yyyy, if not specified), * EEE MMM d HH:mm:ss z yyyy * EEE, d MMM yyyy HH:mm:ss Z
    git-svn-id: https://anonsvn.jboss.org/repos/common/trunk@813 15291643-af16-0410-b526-a589213603f9

--- src/main/org/jboss/util/propertyeditor/DateEditor.java
+++ src/main/org/jboss/util/propertyeditor/DateEditor.java
@@ -11,35 +11,65 @@ package org.jboss.util.propertyeditor;
 
 import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import org.jboss.util.NestedRuntimeException;
 
 /**
- * A property editor for {@link Date}.
+ * A property editor for {@link java.util.Date}.
  *
  * @version <tt>$Revision$</tt>
  * @author  <a href="mailto:jason@planet57.com">Jason Dillon</a>
  * @author  <a href="mailto:d_jencks@users.sourceforge.net">David Jencks</a>
+ * @author Scott.Stark@jboss.org
  */
 public class DateEditor
    extends TextPropertyEditorSupport
 {
+   static DateFormat[] formats;
+
+   static
+   {
+      String defaultFormat = System.getProperty("org.jboss.util.propertyeditor.DateEditor.format",
+         "MMM d, yyyy");
+      formats = new DateFormat[] {
+         new SimpleDateFormat(defaultFormat),
+         // Tue Jan 04 00:00:00 PST 2005
+         new SimpleDateFormat("EEE MMM d HH:mm:ss z yyyy"),
+         // Wed, 4 Jul 2001 12:08:56 -0700
+         new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z")
+      };
+   }
+
    /**
-    * Returns a Date for the input object converted to a string.
-    *
+    * Returns a Date for the input object converted to a string. It tries
+    * the default date format as specified by the following SimpleDateFormat
+    * formats in order:
+    * 
+    * org.jboss.util.propertyeditor.DateEditor.format system property
+    * (or MMM d, yyyy, if not specified),
+    * EEE MMM d HH:mm:ss z yyyy
+    * EEE, d MMM yyyy HH:mm:ss Z
     * @return a Date object
     *
     */
    public Object getValue()
    {
-      try 
+      int n = 0;
+      ParseException ex = null;
+      do
       {
-         DateFormat df = DateFormat.getDateInstance();
-         return df.parse(getAsText());
-      }
-      catch (ParseException e)
-      {
-         throw new NestedRuntimeException(e);
-      }
+         try
+         {
+            DateFormat df = formats[n];
+            return df.parse(getAsText());
+         }
+         catch (ParseException e)
+         {
+            ex = e;
+         }
+         n ++;
+      } while( n < formats.length );
+      throw new NestedRuntimeException(ex);
    }
 }

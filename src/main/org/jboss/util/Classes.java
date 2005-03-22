fd5390e5cf2c0896744279d3a3f3bffb12ac6bc4
ejort <ejort@mycompany.com>
2005-03-22 00:50:22 +0000

    Move displayClassInfo to common/Classes
    git-svn-id: https://anonsvn.jboss.org/repos/common/trunk@950 15291643-af16-0410-b526-a589213603f9

--- src/main/org/jboss/util/Classes.java
+++ src/main/org/jboss/util/Classes.java
@@ -13,7 +13,10 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.Array;
 import java.lang.NoSuchMethodException;
+import java.net.URL;
 
+import java.security.CodeSource;
+import java.security.ProtectionDomain;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -36,6 +39,87 @@ public final class Classes
 
    /** The default package name. */
    public static final String DEFAULT_PACKAGE_NAME = "<default>";
+
+   /** Format a string buffer containing the Class, Interfaces, CodeSource,
+    and ClassLoader information for the given object clazz.
+
+    @param clazz the Class
+    @param results - the buffer to write the info to
+    */
+   public static void displayClassInfo(Class clazz, StringBuffer results)
+   {
+      // Print out some codebase info for the ProbeHome
+      ClassLoader cl = clazz.getClassLoader();
+      results.append("\n"+clazz.getName()+"("+Integer.toHexString(clazz.hashCode())+").ClassLoader="+cl);
+      ClassLoader parent = cl;
+      while( parent != null )
+      {
+         results.append("\n.."+parent);
+         URL[] urls = getClassLoaderURLs(parent);
+         int length = urls != null ? urls.length : 0;
+         for(int u = 0; u < length; u ++)
+         {
+            results.append("\n...."+urls[u]);
+         }
+         if( parent != null )
+            parent = parent.getParent();
+      }
+      CodeSource clazzCS = clazz.getProtectionDomain().getCodeSource();
+      if( clazzCS != null )
+         results.append("\n++++CodeSource: "+clazzCS);
+      else
+         results.append("\n++++Null CodeSource");
+
+      results.append("\nImplemented Interfaces:");
+      Class[] ifaces = clazz.getInterfaces();
+      for(int i = 0; i < ifaces.length; i ++)
+      {
+         Class iface = ifaces[i];
+         results.append("\n++"+iface+"("+Integer.toHexString(iface.hashCode())+")");
+         ClassLoader loader = ifaces[i].getClassLoader();
+         results.append("\n++++ClassLoader: "+loader);
+         ProtectionDomain pd = ifaces[i].getProtectionDomain();
+         CodeSource cs = pd.getCodeSource();
+         if( cs != null )
+            results.append("\n++++CodeSource: "+cs);
+         else
+            results.append("\n++++Null CodeSource");
+      }
+   }
+
+   /** Use reflection to access a URL[] getURLs or URL[] getClasspath method so
+    that non-URLClassLoader class loaders, or class loaders that override
+    getURLs to return null or empty, can provide the true classpath info.
+    */
+   public static URL[] getClassLoaderURLs(ClassLoader cl)
+   {
+      URL[] urls = {};
+      try
+      {
+         Class returnType = urls.getClass();
+         Class[] parameterTypes = {};
+         Class clClass = cl.getClass();
+         Method getURLs = clClass.getMethod("getURLs", parameterTypes);
+         if( returnType.isAssignableFrom(getURLs.getReturnType()) )
+         {
+            Object[] args = {};
+            urls = (URL[]) getURLs.invoke(cl, args);
+         }
+         if( urls == null || urls.length == 0 )
+         {
+            Method getCp = clClass.getMethod("getClasspath", parameterTypes);
+            if( returnType.isAssignableFrom(getCp.getReturnType()) )
+            {
+               Object[] args = {};
+               urls = (URL[]) getCp.invoke(cl, args);               
+            }
+         }
+      }
+      catch(Exception ignore)
+      {
+      }
+      return urls;
+   }
 
    /**
     * Describe the class of an object

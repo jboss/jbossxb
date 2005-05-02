51a0113edb2bd7bda381b5d4afc71bc02e97a35f
starksm <starksm@mycompany.com>
2005-05-02 01:49:18 +0000

    Fix the formatting
    git-svn-id: https://anonsvn.jboss.org/repos/common/trunk@1000 15291643-af16-0410-b526-a589213603f9

--- src/main/org/jboss/util/propertyeditor/ShortEditor.java
+++ src/main/org/jboss/util/propertyeditor/ShortEditor.java
@@ -1,51 +1,27 @@
-/***************************************
-
- *                                     *
-
- *  JBoss: The OpenSource J2EE WebOS   *
-
- *                                     *
-
- *  Distributable under LGPL license.  *
-
- *  See terms of license at gnu.org.   *
-
- *                                     *
-
- ***************************************/
-
+/*
+ * JBoss, Home of Professional Open Source
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
 
 package org.jboss.util.propertyeditor;
 
-
 import java.beans.PropertyEditorSupport;
 
-
-/**
- * A property editor for {@link Integer}.
+/** A property editor for {@link java.lang.Short}.
  *
  * @author Scott.Stark@jboss.org
  * @version $Revision$
  */
-
 public class ShortEditor extends PropertyEditorSupport
-
 {
-
-   /**
-    * Map the argument text into a Short using Short.decode.
+   /** Map the argument text into and Short using Short.decode.
     */
-
    public void setAsText(final String text)
-
    {
-
       Object newValue = Short.decode(text);
-
       setValue(newValue);
-
    }
 
-
 }
-

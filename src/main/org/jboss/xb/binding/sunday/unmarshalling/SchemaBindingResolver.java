e39c0f7fceeb83ff34a513c763ae137db3a4d460
starksm <starksm@mycompany.com>
2005-07-23 03:57:01 +0000

    Add: @param baseURI - an optional baseURI for resolving the schemaLocation. @param schemaLocation - the option schema location uri that matches nsUri if one exists
    git-svn-id: https://anonsvn.jboss.org/repos/common/trunk@1221 15291643-af16-0410-b526-a589213603f9

--- src/main/org/jboss/xb/binding/sunday/unmarshalling/SchemaBindingResolver.java
+++ src/main/org/jboss/xb/binding/sunday/unmarshalling/SchemaBindingResolver.java
@@ -15,10 +15,15 @@ public interface SchemaBindingResolver
    /**
     * Returns an instance of SchemaBinding corresponding to the namespace URI.
     *
-    * @param nsUri  namespace URI
-    * @param localName  the local name of the root element
-    * @return  an instance of SchemaBinding correspnding to the namespace URI or null
-    * if the namespace URI is not recognized (though, in this case it could also throw an exception)
+    * @param nsUri - namespace URI of the element with the schema reference
+    * @param localName - the local name of the element
+    * @param baseURI - an optional baseURI for resolving the schemaLocation.
+    * @param schemaLocation - the option schema location uri that matches
+    *    nsUri if one exists
+    * @return an instance of SchemaBinding correspnding to the namespace URI
+    * or null if the namespace URI is not recognized (though, in this case it
+    * could also throw an exception)
     */
-   SchemaBinding resolve(String nsUri, String localName);
+   SchemaBinding resolve(String nsUri, String localName,
+      String baseURI, String schemaLocation);
 }

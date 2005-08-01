d951f0e99c46170ffbfe64c99285fcf290c64228
dimitris <dimitris@mycompany.com>
2005-08-01 16:28:48 +0000

    tidy up before making any change
    git-svn-id: https://anonsvn.jboss.org/repos/common/trunk@1258 15291643-af16-0410-b526-a589213603f9

--- src/main/org/jboss/net/protocol/file/FileURLLister.java
+++ src/main/org/jboss/net/protocol/file/FileURLLister.java
@@ -1,19 +1,15 @@
-/***************************************
- *                                     *
- *  JBoss: The OpenSource J2EE WebOS   *
- *                                     *
- *  Distributable under LGPL license.  *
- *  See terms of license at gnu.org.   *
- *                                     *
- ***************************************/
-
-
+/*  
+ * JBoss, Home of Professional Open Source
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
 package org.jboss.net.protocol.file;
 
 import java.io.File;
 import java.io.FileFilter;
-import java.io.IOException;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -21,110 +17,115 @@ import java.util.Collection;
 
 import org.jboss.net.protocol.URLListerBase;
 
+/**
+ * FileURLLister
+ *
+ * @author jboynes@users.sf.net
+ * @version $Revision$
+ */
 public class FileURLLister extends URLListerBase
 {
-   public Collection listMembers (final URL baseUrl, final URLFilter filter) throws IOException   
+   public Collection listMembers(URL baseUrl, URLFilter filter) throws IOException   
    {
-      return listMembers (baseUrl, filter, false);
+      return listMembers(baseUrl, filter, false);
    }
 
-   public Collection listMembers (final URL baseUrl, final URLFilter filter, boolean scanNonDottedSubDirs) throws IOException
+   public Collection listMembers(URL baseUrl, URLFilter filter, boolean scanNonDottedSubDirs) throws IOException
    {
-      File directory = new File (baseUrl.getPath ());
+      File directory = new File(baseUrl.getPath());
+      
       if (directory.exists () == false)
       {
-         throw new FileNotFoundException (directory.toString ());
+         throw new FileNotFoundException(directory.toString());
       }
-      return listFiles (baseUrl, filter, scanNonDottedSubDirs);
-      
+      return listFiles(baseUrl, filter, scanNonDottedSubDirs);
    }
    
-   protected Collection listFiles (URL baseUrl, final URLFilter filter, boolean scanNonDottedSubDirs)
+   protected Collection listFiles(URL baseUrl, final URLFilter filter, boolean scanNonDottedSubDirs)
    {      
       // List files at the current dir level
-      //
-      final File directory = new File (baseUrl.getPath ());      
+      final File directory = new File(baseUrl.getPath());      
+
       File[] files = directory.listFiles (new FileFilter ()
       {
-         public boolean accept (File file)
+         public boolean accept(File file)
          {
             try
             {
-               return filter.accept (directory.toURL (), file.getName ());
+               return filter.accept(directory.toURL(), file.getName());
             }
             catch (Exception forgetIt)
             {
-               forgetIt.printStackTrace ();
+               forgetIt.printStackTrace();
                return true;
             }
          }
       });            
       
       // Do we have to scan sub-dirs as well?
-      //
       if (scanNonDottedSubDirs)
       {
-         ArrayList result = new ArrayList ();
+         ArrayList result = new ArrayList();
          
          for (int i = 0; i < files.length; i++)
          {
             File file = files[i];
             
-            if (doesDirTriggersRecursiveSearch (file))
+            if (doesDirTriggersRecursiveSearch(file))
             {  
-               result.addAll (listFiles (prependDirToUrl (baseUrl, file.getName (), true), filter, scanNonDottedSubDirs));
+               result.addAll(listFiles(prependDirToUrl(baseUrl, file.getName(), true), filter, scanNonDottedSubDirs));
             }
             else
             {               
-               result.add ( fileToURL (baseUrl, file) );
+               result.add(fileToURL(baseUrl, file));
             }
          }         
-         
          return result;
       }
       else
       {
          // We don't scan sub-dirs ==> we simply add the URL to the result (after transformation)
-         //
          return filesToURLs(baseUrl, files);
       }
             
    }
    
-   protected boolean doesDirTriggersRecursiveSearch (File file)
+   protected boolean doesDirTriggersRecursiveSearch(File file)
    {
       // File is a directory and contains NO "." (which would means it 
       // could be an exploded package that needs to be deployed i.e. .war, etc.)
-      //
-      return file.isDirectory () && (file.getName ().indexOf (".") == -1);
+
+      return file.isDirectory() && (file.getName().indexOf(".") == -1);
    }
    
-   protected URL prependDirToUrl (URL baseUrl, String addenda, boolean isDirectory)
+   protected URL prependDirToUrl(URL baseUrl, String addenda, boolean isDirectory)
    {
       try
       {
-         String base = baseUrl.toString ();
-         return new URL (base + (base.endsWith ("/") ? "" : "/" ) + addenda + (isDirectory ? "/" : "") );
+         String base = baseUrl.toString();
+         
+         return new URL(base + (base.endsWith("/") ? "" : "/" ) + addenda + (isDirectory ? "/" : ""));
       } 
       catch (MalformedURLException e)
       {
          // shouldn't happen
-         throw new IllegalStateException ();
+         throw new IllegalStateException();
       }
    }
    
-   protected Collection filesToURLs (final URL baseUrl, File[] files)
+   protected Collection filesToURLs(URL baseUrl, File[] files)
    {
       ArrayList result = new ArrayList (files.length);
       
       for (int i = 0; i < files.length; i++)
+      {
          result.add (fileToURL (baseUrl, files[i]));
-      
+      }
       return result;
    }
       
-   protected URL fileToURL (final URL baseUrl, File file)
+   protected URL fileToURL(final URL baseUrl, File file)
    {
-      return prependDirToUrl (baseUrl, file.getName (), file.isDirectory ());         
+      return prependDirToUrl(baseUrl, file.getName(), file.isDirectory ());         
    }
 }

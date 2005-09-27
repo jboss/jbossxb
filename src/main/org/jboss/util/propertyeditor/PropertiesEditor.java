58af1fd0b176d6e8024ea14bf76fbb58a9fbf523
genman <genman@mycompany.com>
2005-09-27 18:40:56 +0000

    Make PropertiesEditor.getAsText() return a string in the same format used for Properties.setAsText()
    git-svn-id: https://anonsvn.jboss.org/repos/common/trunk@1385 15291643-af16-0410-b526-a589213603f9

--- src/main/org/jboss/util/propertyeditor/PropertiesEditor.java
+++ src/main/org/jboss/util/propertyeditor/PropertiesEditor.java
@@ -6,8 +6,12 @@
  */
 package org.jboss.util.propertyeditor;
 
+import java.beans.PropertyEditorSupport;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.StringWriter;
+import java.io.PrintWriter;
 import java.util.Iterator;
 import java.util.Properties;
 
@@ -21,10 +25,16 @@ import org.jboss.util.StringPropertyReplacer;
  * @author Scott.Stark@jboss.org
  * @version $Revision$
  */
-public class PropertiesEditor extends TextPropertyEditorSupport
+public class PropertiesEditor extends PropertyEditorSupport
 {
+
    /**
-    * Returns a Properties object initialized with current getAsText value
+    * To be consistent with the Properties.load and .store methods.
+    */
+   private static final String ENC = "ISO-8859-1";
+
+   /**
+    * Initializes a Properties object with text value
     * interpretted as a .properties file contents. This replaces any
     * references of the form ${x} with the corresponding system property. 
     *
@@ -32,14 +42,13 @@ public class PropertiesEditor extends TextPropertyEditorSupport
     *
     * @throws NestedRuntimeException  An IOException occured.
     */
-   public Object getValue()
+   public void setAsText(String propsText)
    {
       try
       {
          // Load the current key=value properties into a Properties object
-         String propsText = getAsText();
          Properties rawProps = new Properties(System.getProperties());
-         ByteArrayInputStream bais = new ByteArrayInputStream(propsText.getBytes());
+         ByteArrayInputStream bais = new ByteArrayInputStream(propsText.getBytes(ENC));
          rawProps.load(bais);
          // Now go through the rawProps and replace any ${x} refs
          Properties props = new Properties();
@@ -53,7 +62,25 @@ public class PropertiesEditor extends TextPropertyEditorSupport
          }
          rawProps.clear();
 
-         return props;
+         setValue(props);
+      }
+      catch (IOException e)
+      {
+         throw new NestedRuntimeException(e);
+      }
+   }
+
+   /**
+    * Returns the properties as text.
+    */
+   public String getAsText()
+   {
+      try
+      {
+         ByteArrayOutputStream baos = new ByteArrayOutputStream();
+         Properties p = (Properties)getValue();
+         p.store(baos, null);
+         return baos.toString(ENC);
       }
       catch (IOException e)
       {

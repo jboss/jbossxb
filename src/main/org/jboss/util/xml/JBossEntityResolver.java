7af766b4f192da82979d08db3db31ebe5b5a9de6
asaldhana <asaldhana@mycompany.com>
2005-12-27 21:50:52 +0000

    Resolve the security_config_5_0.xsd
    git-svn-id: https://anonsvn.jboss.org/repos/common/trunk@1548 15291643-af16-0410-b526-a589213603f9

--- src/main/org/jboss/util/xml/JBossEntityResolver.java
+++ src/main/org/jboss/util/xml/JBossEntityResolver.java
@@ -124,6 +124,7 @@ public class JBossEntityResolver implements EntityResolver
       registerEntity("http://www.jboss.org/j2ee/schema/security-config_4_0.xsd", "security-config_4_0.xsd");
       registerEntity("urn:jboss:bean-deployer", "bean-deployer_1_0.xsd");
       registerEntity("urn:jboss:security-config:4.1", "security-config_4_1.xsd");
+      registerEntity("urn:jboss:security-config:5.0", "security-config_5_0.xsd");
       registerEntity("urn:jboss:jndi-binding-service:1.0", "jndi-binding-service_1_0.xsd");
       registerEntity("urn:jboss:user-roles:1.0", "user-roles_1_0.xsd");
       // xml

e216acc058af32290b52578f56ed589e6be85612
dimitris <dimitris@mycompany.com>
2006-01-19 14:47:15 +0000

    JBAS-2679, retrieve org.jboss.net.protocol.file.decodeFilePaths property properly.
    git-svn-id: https://anonsvn.jboss.org/repos/common/trunk@1580 15291643-af16-0410-b526-a589213603f9

--- src/main/org/jboss/net/protocol/file/FileURLConnection.java
+++ src/main/org/jboss/net/protocol/file/FileURLConnection.java
@@ -1,24 +1,24 @@
 /*
-  * JBoss, Home of Professional Open Source
-  * Copyright 2005, JBoss Inc., and individual contributors as indicated
-  * by the @authors tag. See the copyright.txt in the distribution for a
-  * full listing of individual contributors.
-  *
-  * This is free software; you can redistribute it and/or modify it
-  * under the terms of the GNU Lesser General Public License as
-  * published by the Free Software Foundation; either version 2.1 of
-  * the License, or (at your option) any later version.
-  *
-  * This software is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-  * Lesser General Public License for more details.
-  *
-  * You should have received a copy of the GNU Lesser General Public
-  * License along with this software; if not, write to the Free
-  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
-  */
+ * JBoss, Home of Professional Open Source
+ * Copyright 2005, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.jboss.net.protocol.file;
 
 import java.io.File;
@@ -42,29 +42,29 @@ import java.io.BufferedInputStream;
  * Provides local file access via URL semantics, correctly returning
  * the last modified time of the underlying file.
  *
- * @version $Revision$
  * @author  <a href="mailto:jason@planet57.com">Jason Dillon</a>
- * @author  Scott.Stark@jboss.org
+ * @author  <a href="mailto:scott.stark@jboss.org">Scott.Stark</a>
+ * @version $Revision$
  */
-public class FileURLConnection
-   extends URLConnection
+public class FileURLConnection extends URLConnection
 {
    static boolean decodeFilePaths = false;
    static
    {
       String flag = System.getProperty("org.jboss.net.protocol.file.decodeFilePaths");
-      if( flag != null )
-         decodeFilePaths = Boolean.getBoolean(flag);
+      if (flag != null)
+         decodeFilePaths = Boolean.valueOf(flag).booleanValue();
    }
+   
    protected File file;
 
-   public FileURLConnection(final URL url)
-      throws MalformedURLException, IOException
+   public FileURLConnection(final URL url) throws MalformedURLException, IOException
    {
       super(url);
       String path = url.getPath();
-      if( decodeFilePaths )
+      if (decodeFilePaths)
          path = URLDecoder.decode(path, "UTF-8");
+      
       // Convert the url '/' to the os file separator
       file = new File(path.replace('/', File.separatorChar).replace('|', ':'));
 
@@ -157,7 +157,8 @@ public class FileURLConnection
       return headerField;
    }
 
-   /** Return a permission for reading of the file
+   /** 
+    * Return a permission for reading of the file
     */
    public Permission getPermission() throws IOException
    {

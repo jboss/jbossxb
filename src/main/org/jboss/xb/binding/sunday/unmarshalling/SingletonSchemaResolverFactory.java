e171f0c683a4c0a3c2fba31fb8b62ac3f53f9f88
adrian <adrian@mycompany.com>
2006-03-14 12:30:57 +0000

    [JBMICROCONT-14] - Move the choice of TypeInfoFactory out of the classadapter. Also removed old aspect/mc integration tests. These need reimplemeting on the new prototype.
    git-svn-id: https://anonsvn.jboss.org/repos/common/trunk@1694 15291643-af16-0410-b526-a589213603f9

--- src/main/org/jboss/xb/binding/sunday/unmarshalling/SingletonSchemaResolverFactory.java
+++ src/main/org/jboss/xb/binding/sunday/unmarshalling/SingletonSchemaResolverFactory.java
@@ -58,7 +58,6 @@ public class SingletonSchemaResolverFactory implements SchemaResolverFactory
     */
    private SingletonSchemaResolverFactory()
    {
-      addSchema("urn:jboss:aop-deployer", "org.jboss.aspects.kernel.schema.AspectSchemaBindingInitalizer", Boolean.FALSE);
       addSchema("urn:jboss:bean-deployer", "org.jboss.kernel.plugins.deployment.xml.BeanSchemaInitializer", Boolean.FALSE);
       addSchema("urn:jboss:bean-deployer:2.0", "org.jboss.kernel.plugins.deployment.xml.BeanSchemaInitializer20", Boolean.FALSE);
       addSchema("urn:jboss:javabean:1.0", "org.jboss.kernel.plugins.config.xml.JavaBeanSchemaInitializer", Boolean.FALSE);

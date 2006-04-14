2614f0b9cbedf8fc6c2eaa48fa5092ae1d09b8c3
starksm <starksm@mycompany.com>
2006-04-14 01:49:30 +0000

    Implement reverse, setLength
    git-svn-id: https://anonsvn.jboss.org/repos/common/trunk@1753 15291643-af16-0410-b526-a589213603f9

--- src/main/org/jboss/util/JBossStringBuilder.java
+++ src/main/org/jboss/util/JBossStringBuilder.java
@@ -22,6 +22,7 @@
 package org.jboss.util;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 /**
  * A JBossStringBuilder, providing the same functionality as the
@@ -31,16 +32,16 @@ import java.io.Serializable;
  * @version $Revision$
  */
 public class JBossStringBuilder implements Serializable, CharSequence
-{ 
+{
    /** Serialization */
    private static final long serialVersionUID = 1874946609763446794L;
 
    /** The characters */
    protected char[] chars;
-   
+
    /** The position */
-   protected int pos; 
-   
+   protected int pos;
+
    /**
     * Create a new StringBuilder with no characters and an intial
     * size of 16
@@ -49,7 +50,7 @@ public class JBossStringBuilder implements Serializable, CharSequence
    {
       this(16);
    }
-   
+
    /**
     * Create a new StringBuilder with no characters
     * 
@@ -59,8 +60,8 @@ public class JBossStringBuilder implements Serializable, CharSequence
    {
       chars = new char[capacity];
    }
-   
-   
+
+
    /**
     * Create a new StringBuilder from the given string.
     * The initial capacity is the length of the string plus 16
@@ -72,7 +73,7 @@ public class JBossStringBuilder implements Serializable, CharSequence
       this(string.length() + 16);
       append(string);
    }
-   
+
    /**
     * Create a new StringBuilder from the given character sequence.
     * The initial capacity is the length of the sequence plus 16
@@ -84,7 +85,7 @@ public class JBossStringBuilder implements Serializable, CharSequence
       this(charSequence.length() + 16);
       append(charSequence);
    }
-   
+
    /**
     * Create a new StringBuilder from the given character array
     * 
@@ -94,7 +95,7 @@ public class JBossStringBuilder implements Serializable, CharSequence
    {
       this(ch, 0, ch.length);
    }
-   
+
    /**
     * Create a new StringBuilder from the given character array
     * 
@@ -107,25 +108,25 @@ public class JBossStringBuilder implements Serializable, CharSequence
       this(length + 16);
       append(ch, start, length);
    }
-   
+
    public JBossStringBuilder append(Object object)
    {
       return append(String.valueOf(object));
    }
-   
+
    public JBossStringBuilder append(String string)
    {
       if (string == null)
          string = "null";
-      
+
       int length = string.length();
       if (length == 0)
          return this;
-      
+
       int afterAppend = pos + length;
       if (afterAppend > chars.length)
          expandCapacity(afterAppend);
-      
+
       string.getChars(0, length, chars, pos);
       pos = afterAppend;
       return this;
@@ -135,15 +136,15 @@ public class JBossStringBuilder implements Serializable, CharSequence
    {
       if (buffer == null)
          return append("null");
-      
+
       int length = buffer.length();
       if (length == 0)
          return this;
-      
+
       int afterAppend = pos + length;
       if (afterAppend > chars.length)
          expandCapacity(afterAppend);
-      
+
       buffer.getChars(0, length, chars, pos);
       pos = afterAppend;
       return this;
@@ -153,11 +154,11 @@ public class JBossStringBuilder implements Serializable, CharSequence
    {
       if (charSequence == null)
          return append("null");
-      
+
       int length = charSequence.length();
       if (length == 0)
          return this;
-      
+
       return append(charSequence, 0, charSequence.length());
    }
 
@@ -172,7 +173,7 @@ public class JBossStringBuilder implements Serializable, CharSequence
       int length = end - start;
       if (length == 0)
          return this;
-      
+
       int afterAppend = pos + length;
       if (afterAppend > chars.length)
          expandCapacity(afterAppend);
@@ -187,7 +188,7 @@ public class JBossStringBuilder implements Serializable, CharSequence
    {
       if (array == null)
          return append("null");
-      
+
       if (array.length == 0)
          return this;
 
@@ -199,11 +200,11 @@ public class JBossStringBuilder implements Serializable, CharSequence
    {
       if (array == null)
          return append("null");
-      
+
       int arrayLength = array.length;
       if (offset < 0 || length < 0 || offset + length > arrayLength)
          throw new IndexOutOfBoundsException("Invalid offset=" + offset + " length=" + length + " array.length=" + arrayLength);
-      
+
       if (length == 0 || arrayLength == 0)
          return this;
 
@@ -254,7 +255,7 @@ public class JBossStringBuilder implements Serializable, CharSequence
 
       if (start == end)
          return this;
-      
+
       int removed = end - start;
       System.arraycopy(chars, start + removed, chars, start, pos - end);
       pos -= removed;
@@ -265,37 +266,37 @@ public class JBossStringBuilder implements Serializable, CharSequence
    {
       return delete(index, 1);
    }
-   
+
    public JBossStringBuilder replace(int start, int end, String string)
    {
       delete(start, end);
       return insert(start, string);
    }
-   
+
    public JBossStringBuilder insert(int index, char[] string)
    {
       return insert(index, string, 0, string.length);
    }
-   
+
    public JBossStringBuilder insert(int index, char[] string, int offset, int len)
    {
       int stringLength = string.length;
       if (index < 0 || index > pos || offset < 0 || len < 0 || (offset + len) > string.length)
          throw new IndexOutOfBoundsException("Invalid index=" + index + " offset=" + offset + " len=" + len + " string.length=" + stringLength + " length=" + pos);
-      
+
       if (len == 0)
          return this;
-      
+
       int afterAppend = pos + len;
       if (afterAppend > chars.length)
          expandCapacity(afterAppend);
-      
+
       System.arraycopy(chars, index, chars, index + stringLength, pos - index);
       System.arraycopy(string, offset, chars, index, len);
       pos = afterAppend;
       return this;
    }
-   
+
    public JBossStringBuilder insert(int offset, Object object)
    {
       if (object == null)
@@ -303,7 +304,7 @@ public class JBossStringBuilder implements Serializable, CharSequence
       else
          return insert(offset, String.valueOf(object));
    }
-   
+
    public JBossStringBuilder insert(int offset, String string)
    {
       if (offset < 0 || offset > pos)
@@ -311,19 +312,19 @@ public class JBossStringBuilder implements Serializable, CharSequence
 
       if (string == null)
          string = "null";
-      
+
       int stringLength = string.length();
-      
+
       int afterAppend = pos + stringLength;
       if (afterAppend > chars.length)
          expandCapacity(afterAppend);
-      
+
       System.arraycopy(chars, offset, chars, offset + stringLength, pos - offset);
       string.getChars(0, stringLength, chars, offset);
       pos = afterAppend;
       return this;
    }
-   
+
    public JBossStringBuilder insert(int offset, CharSequence charSequence)
    {
       if (charSequence == null)
@@ -331,110 +332,114 @@ public class JBossStringBuilder implements Serializable, CharSequence
       else
          return insert(offset, charSequence, 0, charSequence.length());
    }
-   
+
    public JBossStringBuilder insert(int offset, CharSequence charSequence, int start, int end)
    {
       if (charSequence == null)
          charSequence = "null";
-      
+
       int sequenceLength = charSequence.length();
       if (offset < 0 || offset > pos || start < 0 || end < 0 || start > sequenceLength || end > sequenceLength || start > end)
          throw new IndexOutOfBoundsException("Invalid offset=" + offset + " start=" + start + " end=" + end + " sequence.length()=" + sequenceLength + " length=" + pos);
-      
+
       int len = end - start;
       if (len == 0)
          return this;
-      
+
       int afterAppend = pos + len;
       if (afterAppend > chars.length)
          expandCapacity(afterAppend);
-      
+
       System.arraycopy(chars, offset, chars, offset + sequenceLength, pos - offset);
       for (int i = start; i < end; ++i)
          chars[offset++] = charSequence.charAt(i);
       pos = afterAppend;
       return this;
    }
-   
+
    public JBossStringBuilder insert(int offset, boolean primitive)
    {
       return insert(offset, String.valueOf(primitive));
    }
-   
+
    public JBossStringBuilder insert(int offset, char primitive)
    {
       return insert(offset, String.valueOf(primitive));
    }
-   
+
    public JBossStringBuilder insert(int offset, int primitive)
    {
       return insert(offset, String.valueOf(primitive));
    }
-   
+
    public JBossStringBuilder insert(int offset, long primitive)
    {
       return insert(offset, String.valueOf(primitive));
    }
-   
+
    public JBossStringBuilder insert(int offset, float primitive)
    {
       return insert(offset, String.valueOf(primitive));
    }
-   
+
    public JBossStringBuilder insert(int offset, double primitive)
    {
       return insert(offset, String.valueOf(primitive));
    }
-   
+
    public int indexOf(String string)
    {
       return indexOf(string, 0);
    }
-   
+
    public int indexOf(String string, int fromIndex)
    {
       // FIXME
       return toString().indexOf(string, fromIndex);
    }
-   
+
    public int indexOf(char ch)
    {
       return indexOf(ch, 0);
    }
-   
+
    public int indexOf(char ch, int fromIndex)
    {
       // FIXME
       return toString().indexOf(ch, fromIndex);
    }
-   
+
    public int lastIndexOf(String string)
    {
       return lastIndexOf(string, pos);
    }
-   
+
    public int lastIndexOf(String string, int fromIndex)
    {
       // FIXME
       return toString().lastIndexOf(string, fromIndex);
    }
-   
+
    public int lastIndexOf(char ch)
    {
       return lastIndexOf(ch, pos);
    }
-   
+
    public int lastIndexOf(char ch, int fromIndex)
    {
       // FIXME
       return toString().lastIndexOf(ch, fromIndex);
    }
-   
+
    public JBossStringBuilder reverse()
    {
-      throw new NotImplementedException("FIXME: NYI");
+      char[] tmp = new char[pos];
+      for(int n = 0; n < pos; n ++)
+         tmp[n] = chars[pos-n-1];
+      chars = tmp;
+      return this;
    }
-   
+
    public String toString()
    {
       return new String(chars, 0, pos);
@@ -449,26 +454,31 @@ public class JBossStringBuilder implements Serializable, CharSequence
    {
       return chars.length;
    }
-   
+
    public void ensureCapacity(int minimum)
    {
       if (minimum < 0 || minimum < chars.length)
          return;
       expandCapacity(minimum);
    }
-   
+
    public void trimToSize()
    {
       char[] trimmed = new char[pos];
       System.arraycopy(chars, 0, trimmed, 0, pos);
       chars = trimmed;
    }
-   
+
    public void setLength(int newLength)
    {
-      throw new NotImplementedException("FIXME: NYI");
+      if (newLength < 0)
+          throw new StringIndexOutOfBoundsException(newLength);
+      if (newLength > chars.length)
+          expandCapacity(newLength);
+      Arrays.fill(chars, newLength, chars.length, '\0');
+      pos = newLength;
    }
-   
+
    public char charAt(int index)
    {
       return chars[index];
@@ -478,40 +488,40 @@ public class JBossStringBuilder implements Serializable, CharSequence
    {
       if (srcBegin < 0 || dstBegin < 0 || srcBegin > srcEnd || srcEnd > pos || (dstBegin + srcEnd - srcBegin) > dst.length)
          throw new IndexOutOfBoundsException("Invalid srcBegin=" + srcBegin + " srcEnd=" + srcEnd + " dstBegin=" + dstBegin + " dst.length=" + dst.length + " length=" + pos);
-      
+
       int len = srcEnd - srcBegin;
       if (len == 0)
          return;
-      
+
       System.arraycopy(chars, srcBegin, dst, dstBegin, len);
    }
-   
+
    public void setCharAt(int index, char ch)
    {
       if (index < 0 || index > pos)
          throw new IndexOutOfBoundsException("Invalid index=" + index + " length=" + pos);
-      
+
       chars[index] = ch;
    }
-   
+
    public String substring(int start)
    {
       return substring(start, pos);
    }
-   
+
    public CharSequence subSequence(int start, int end)
    {
       return substring(start, end);
    }
-   
+
    public String substring(int start, int end)
    {
       if (start < 0 || end < 0 || start > end || end > pos)
          throw new IndexOutOfBoundsException("Invalid start=" + start + " end=" + end + " length=" + pos);
-      
+
       return new String(chars, start, end - start);
    }
-   
+
    /**
     * Expand the capacity to the greater of the minimum
     * or twice the current size
@@ -523,7 +533,7 @@ public class JBossStringBuilder implements Serializable, CharSequence
       int newSize = chars.length * 2;
       if (minimum > newSize)
          newSize = minimum;
-      
+
       char[] newChars = new char[newSize];
       System.arraycopy(chars, 0, newChars, 0, pos);
       chars = newChars;

0d389b6df2b7351ac84bd0e7a489ebd19dfe1bfb
alesj <alesj@mycompany.com>
2007-01-25 10:13:17 +0000

    Added SpringSchemaInitializer def.
    git-svn-id: https://anonsvn.jboss.org/repos/common/jbossxb/trunk/src@2250 15291643-af16-0410-b526-a589213603f9

--- main/java/org/jboss/xb/binding/sunday/unmarshalling/SingletonSchemaResolverFactory.java
+++ main/java/org/jboss/xb/binding/sunday/unmarshalling/SingletonSchemaResolverFactory.java
@@ -63,6 +63,7 @@ public class SingletonSchemaResolverFactory implements SchemaResolverFactory
       addSchema("urn:jboss:bean-deployer:2.0", "org.jboss.kernel.plugins.deployment.xml.BeanSchemaInitializer20", Boolean.FALSE);
       addSchema("urn:jboss:javabean:1.0", "org.jboss.kernel.plugins.config.xml.JavaBeanSchemaInitializer", Boolean.FALSE);
       addSchema("urn:jboss:javabean:2.0", "org.jboss.kernel.plugins.config.xml.JavaBeanSchemaInitializer20", Boolean.FALSE);
+      addSchema("urn:jboss:spring-beans:2.0", "org.jboss.spring.deployment.xml.SpringSchemaInitializer", Boolean.FALSE);
    }
 
    public SchemaBindingResolver getSchemaBindingResolver()

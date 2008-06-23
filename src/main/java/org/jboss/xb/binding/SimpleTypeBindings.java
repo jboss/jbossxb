b6c9dbdac29ae8dac071583b77346830ca79e496
Darran Lofthouse <darran.lofthouse@jboss.com>
2008-06-23 15:34:22 +0000

    [JBXB-141] Revert invalid change.
    git-svn-id: https://anonsvn.jboss.org/repos/common/jbossxb/branches/1_0@2875 15291643-af16-0410-b526-a589213603f9

--- src/main/java/org/jboss/xb/binding/SimpleTypeBindings.java
+++ src/main/java/org/jboss/xb/binding/SimpleTypeBindings.java
@@ -200,7 +200,7 @@ public final class SimpleTypeBindings
 
       public String marshal(Object value)
       {
-         return marshallDouble((Double)value);
+         return String.valueOf(value);
       }
    };
 
@@ -1079,7 +1079,19 @@ public final class SimpleTypeBindings
       }
       else if(typeCode == XS_DOUBLE)
       {
-         result = marshallDouble((Double)value);
+         Double d = (Double)value;
+         if(d.doubleValue() == Double.POSITIVE_INFINITY)
+         {
+            result = "INF";
+         }
+         else if(d.doubleValue() == Double.NEGATIVE_INFINITY)
+         {
+            result = "-INF";
+         }
+         else
+         {
+            result = d.toString();
+         }
       }
       else if(typeCode == XS_BOOLEAN)
       {
@@ -2247,31 +2259,6 @@ public final class SimpleTypeBindings
       return result;
    }
 
-   private static String marshallDouble(Double d)
-   {	   
-      String result;
-      if(d.doubleValue() == Double.POSITIVE_INFINITY)
-      {
-         result = "INF";
-      }
-      else if(d.doubleValue() == Double.NEGATIVE_INFINITY)
-      {
-         result = "-INF";
-      }
-      else
-      {
-         DecimalFormat df = new DecimalFormat();
-         df.setGroupingUsed(false);
-         df.setMinimumFractionDigits(1);
-         df.setMaximumFractionDigits(18);
-         df.setMinimumIntegerDigits(1);
-         df.setMaximumIntegerDigits(18);
-         
-         result = df.format(d.doubleValue());
-      }
-      return result;	   
-   }
-   
    private static char convertDigit(int value)
    {
       value &= 0x0f;
